82467a2b7049bd3c15880a4a5b5884a0e96e3063
Ayush Tyagi <ayush.tyagi145@gmail.com>
2024-04-04 01:06:49 +0530

    get all clubs and get club details

--- backend/controllers/clubs.js
+++ backend/controllers/clubs.js
@@ -1,12 +1,29 @@
 // controllers/documentController.js
 const { supabase } = require("../db");
 
-async function fetchDocument(req, res) {
+async function fetchClubs(req, res) {
   try {
-    const { table, id } = req.params; // Correct variable name from tableName to table
     const { data: document, error } = await supabase
-      .from(table) // Use table instead of tableName
+      .from("Clubs") // Use table instead of tableName
       .select("*")
+
+    if (error) {
+      throw error;
+    }
+    res.json(document);
+  } catch (error) {
+    console.error("Error fetching Clubs:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+async function fetchDetails(req, res) {
+  try {
+    const { type, id } = req.params; // Correct variable name from tableName to table
+    const { data: document, error } = await supabase
+      .from("Clubs") // Use table instead of tableName
+      .select("*")
+      .filter('type', 'eq', type)
       .eq("clubId", id)
       .single();
 
@@ -20,4 +37,4 @@ async function fetchDocument(req, res) {
   }
 }
 
-module.exports = { fetchDocument };
+module.exports = { fetchDetails, fetchClubs };
--- backend/routes/clubs.js
+++ backend/routes/clubs.js
@@ -1,9 +1,10 @@
 // routes.js
 const express = require("express");
 const router = express.Router();
-const { fetchDocument } = require("../controllers/clubs");
+const { fetchDetails, fetchClubs } = require("../controllers/clubs");
 
 // Route to get a single document from a specified table
-router.get("/details/:table/:id", fetchDocument);
+router.get("/details/:type/:id", fetchDetails);
+router.get("/getAllClubs", fetchClubs)
 
 module.exports = router;
